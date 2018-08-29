82d3b7ff0aa5abc83c5f7e77219f4282940c3197
Shterion <shterion.yanev@yahoo.com>
2018-08-29 11:05:11 +0200

    Add inital tests for creating a session in wice

---
+++ spec/seed/seed.js
@@ -0,0 +1,9 @@
+const configOptions = {
+  client_name: "sandbox",
+  username: "syanev ",
+  password: "d36adb53",
+  path: "https://oihwice.wice-net.de",
+  apikey: "m3n8qxl42fmxlhqln8c6rwblb4x9m51r",
+};
+
+module.exports = { configOptions };
