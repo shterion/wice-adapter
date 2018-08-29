f5c75eb897978c1277fe36c2e97f9a4c2ba2f863
Shterion <shterion.yanev@yahoo.com>
2018-08-29 13:08:34 +0200

    Refactorin: modify upserPerson action

--- spec/seed/seed.js
+++ spec/seed/seed.js
@@ -6,4 +6,50 @@ const configOptions = {
   apikey: "m3n8qxl42fmxlhqln8c6rwblb4x9m51r",
 };
 
+const organizations = [{
+    body: {
+      name: 'Company Ltd.',
+      email: 'info@company.com'
+    }
+  },
+  {
+    body: {
+      name: 'Test GmbH',
+      email: 'info@testgmbh.com'
+    }
+  },
+  {
+    body: {
+      name: 'Travel Mates',
+      email: 'info@travelmates.com'
+    }
+  }
+];
+
+const persons = [{
+    body: {
+      name: 'Brown',
+      firstname: 'Adam'
+    }
+  },
+  {
+    body: {
+      name: 'Doe',
+      firstname: 'John'
+    }
+  },
+  {
+    body: {
+      name: 'Kolarov',
+      firstname: 'Sebastian',
+      email: 'kolarov@mail.com'
+    }
+  },
+];
+
+module.exports = {
+  configOptions,
+  persons,
+  organizations
+}
 module.exports = { configOptions };
