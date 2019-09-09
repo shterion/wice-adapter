4ecb6b5f0e66a0164c521db239cc7492c09cbbe4
Shterion <shterion.yanev@yahoo.com>
2019-09-09 12:43:55 +0200

    Revert links to old server

--- spec/seed/seed.js
+++ spec/seed/seed.js
@@ -2,81 +2,81 @@ const configOptions = {
   client_name: 'sandbox',
   username: 'shterion ',
   password: 'd36adb53',
-  path: 'http://oihwice2.wice-net.de',
-  apikey: 'fsuogsi9p1im1gpnhvapjdtx94z46qye'
+  path: 'https://oihwice.wice-net.de',
+  apikey: 'fsuogsi9p1im1gpnhvapjdtx94z46qye',
 };
 
 const options = {
   method: 'POST',
-  uri: 'http://oihwice2.wice-net.de/plugin/wp_wice_client_api_backend/json',
+  uri: 'https://oihwice.wice-net.de/plugin/wp_wice_client_api_backend/json',
   headers: {
-    'X-API-KEY': undefined // 'fsuogsi9p1im1gpnhvapjdtx94z46qye'
-  }
+    'X-API-KEY': undefined, // 'fsuogsi9p1im1gpnhvapjdtx94z46qye'
+  },
 };
 
 const organizations = [{
   body: {
     name: 'Company Ltd.',
-    email: 'info@company.com'
-  }
+    email: 'info@company.com',
+  },
 },
 {
   body: {
     name: 'Test GmbH',
-    email: 'info@testgmbh.com'
-  }
+    email: 'info@testgmbh.com',
+  },
 },
 {
   body: {
     name: 'Travel Mates',
-    email: 'info@travelmates.com'
-  }
-}
+    email: 'info@travelmates.com',
+  },
+},
 ];
 
 const persons = [{
   body: {
     name: 'Brown',
-    firstname: 'Adam'
-  }
+    firstname: 'Adam',
+  },
 },
 {
   body: {
     name: 'Doe',
-    firstname: 'John'
-  }
+    firstname: 'John',
+  },
 },
 {
   body: {
     name: 'Kolarovv',
     firstname: 'Sebastian',
-    email: 'kolarov@mail.com'
-  }
-}
+    email: 'kolarov@mail.com',
+  },
+},
 ];
 
 const articles = [{
   body: {
-    description: 'Arduino Nano'
-  }
+    description: 'Arduino Nano',
+  },
 },
 {
   body: {
-    description: 'Raspberry Pi'
-  }
-}
+    description: 'Raspberry Pi',
+  },
+},
 ];
 
 module.exports = {
   configOptions,
   persons,
   organizations,
-  articles
+  articles,
 };
 module.exports = {
   options,
   configOptions,
   persons,
   organizations,
-  articles
+  articles,
 };
